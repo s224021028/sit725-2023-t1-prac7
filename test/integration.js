a5c06505d5c1dce6a6e3e1199af930950b30d425
Roy97 <rd062595@gmail.com>
2023-12-26 14:55:42 +1100

    updated files

--- test/integration.js
+++ test/integration.js
@@ -1,6 +1,6 @@
 const chai = require("chai")
 const chaiHttp = require("chai-http")
-const app = require("../server").app
+const app = require("../server")
 const expect = chai.expect
 
 chai.use(chaiHttp)
